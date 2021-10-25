a811ecbcaf5281570e4764b226d42e5e5b90ef0e
Galvs135 <lg_franca@hotmail.com>
2021-10-25 09:28:16 -0300

    subindo alterações para o repositorio

--- src/katas2.js
+++ src/katas2.js
@@ -1,36 +1,66 @@
-// comece a criar a sua função add na linha abaixo
-
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
-
-
-// comece a criar a sua função multiply na linha abaixo
+function add(valor1 = 2, valor2 = 4)
+{
+    return valor1+valor2;
+}
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+
+function multiply(number1 = 4,number2 = 6)
+{
+    let resultado = 0;
+    for(counter = 1; counter <= number2; counter++) 
+    { 
+        resultado = add(resultado, number1);   
+    }
+    return resultado;       
+}
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
-// comece a criar a sua função power na linha abaixo
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
-// comece a criar a sua função factorial na linha abaixo
+function power(x = 2,n = 8)
+{
+    let resultado = 1;
+    for(counter2 = 1; counter2 <= n; counter2 ++)
+    {
+        resultado = multiply(resultado,x);
+    }
+    return resultado;
+}
+ console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
 
-// crie a função fibonacci
+function factorial(numeral = 5)
+{
+    let resultado = 1;
+    for(counter3 = 1; counter3 <= numeral; counter3 ++)
+    {
+        resultado = multiply(resultado,counter3)
+    }
+  return resultado  
+}
+ console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
+
+
+
+function fibonacci(n = 8)
+{
+    let array = [];
+    let result= 0;
+    for(counter = 0;counter <= n; counter ++)
+    {
+        result = array.indexOf[counter] + (array.indexOf[counter-1]-1);
+        array.push(result)     
+    }
+    return array.indexOf(n)
+}
+ console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
